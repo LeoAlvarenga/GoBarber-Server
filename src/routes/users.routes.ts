0ddf5d5958d36e81f4cdca1f38008cbc35538fee
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-06-30 20:18:42 -0300

    creating update avatar route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,26 +1,36 @@
-import { Router } from "express";
+import { Router, request, response } from "express";
 import CreateUserService from "../services/CreateUserService";
+import multer from 'multer'
+import uploadConfig from '../config/upload'
+
+import ensureAuthentication from '../middleware/ensureAuthentication'
 
 const usersRouter = Router();
+const upload = multer(uploadConfig)
+
 
 usersRouter.post("/", async (request, response) => {
   try {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService()
+    const createUser = new CreateUserService();
 
     const user = await createUser.execute({
-        name,
-        email,
-        password
-    })
+      name,
+      email,
+      password,
+    });
 
-    delete user.password
+    delete user.password;
 
-    return response.json(user)
+    return response.json(user);
   } catch (err) {
     return response.status(400).json({ message: err.message });
   }
 });
+
+usersRouter.patch('/avatar',ensureAuthentication, upload.single('avatar'), async (request, response) => {
+  response.json({ message: 'ok' })
+})
 
 export default usersRouter;
